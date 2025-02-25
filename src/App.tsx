aa2561a0ee5f55bc310a7511fc65a01300b67f76
daseul <7581058@gmail.com>
2025-02-26 05:29:06 +0900

    ✨ feat(router): 라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { css } from '@emotion/react'
+import { RouterProvider } from 'react-router'
+
+import router from './router'
+
 function App() {
-  return <></>
+  return (
+    <div css={ContainerStyle}>
+      <RouterProvider router={router} />
+    </div>
+  )
 }
 
 export default App
+
+const ContainerStyle = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+`
---
+++ src/router/index.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, Navigate } from 'react-router'
+
+import MainLayout from '../layouts/MainLayout'
+import CreatePage from '../pages/CreatePage'
+import ListPage from '../pages/ListPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to="/list" />,
+      },
+      {
+        path: 'list',
+        element: <ListPage />,
+      },
+      {
+        path: 'create',
+        element: <CreatePage />,
+      },
+    ],
+  },
+])
+export default router
