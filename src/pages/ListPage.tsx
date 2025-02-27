c8ee38dfe03d562baee41062e9a119f74c6bd1a2
daseul <7581058@gmail.com>
2025-02-28 01:35:16 +0900

    ✨ feat(list): 설문조사 목록 테이블 추가

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,5 +1,90 @@
+import { Table, TableProps } from 'antd'
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+import useSWR from 'swr'
+
+import { PAGE_SIZE } from '../constants'
+import { fetcher } from '../lib/fetcher'
+
+interface DataItemType {
+  createdAt: string
+  id: number
+  title: string
+}
+interface DataType extends DataItemType {
+  action: number
+}
+
+const columns: TableProps<DataType>['columns'] = [
+  {
+    title: '번호',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: '제목',
+    dataIndex: 'title',
+    key: 'title',
+  },
+  {
+    title: '생성일',
+    dataIndex: 'createdAt',
+    key: 'createdAt',
+    render: (date: string) => {
+      const time = new Date(date)
+      return `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`
+    },
+  },
+  {
+    title: '액션',
+    key: 'action',
+    dataIndex: 'id',
+    render: (id: number) => {
+      return (
+        <button
+          onClick={(e) => {
+            e.stopPropagation()
+            alert(`삭제 ${id}`)
+          }}
+        >
+          삭제
+        </button>
+      )
+    },
+  },
+]
+
 const ListPage = () => {
-  return <div>ListPage</div>
+  const navigate = useNavigate()
+  const [page, setPage] = useState<number | undefined>(2)
+  const { data, error } = useSWR('/surveys', fetcher)
+  if (error) {
+    return 'error'
+  }
+  return (
+    <div>
+      <Table
+        columns={columns}
+        dataSource={data?.map((item: DataItemType) => ({
+          ...item,
+          key: item.id,
+        }))}
+        onRow={(record) => {
+          return {
+            onClick: () => {
+              navigate(`/create/${record.id}`)
+            },
+          }
+        }}
+        pagination={{
+          total: data?.length,
+          current: page,
+          pageSize: PAGE_SIZE,
+        }}
+        onChange={(pagination) => setPage(pagination.current)}
+      />
+    </div>
+  )
 }
 
 export default ListPage
