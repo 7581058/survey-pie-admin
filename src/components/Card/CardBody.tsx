15f7e8fda9e3c61ba2cd292f4aa3bd042fd70abc
daseul <7581058@gmail.com>
2025-03-11 00:46:12 +0900

    ✨ feat(create): 설문조사 생성 카드 바디 컴포넌트 추가

---
+++ src/components/Card/CardBody.tsx
@@ -0,0 +1,23 @@
+import { QuestionOptionsType } from '../../types'
+import SelectInput from '../Input/SelectInput'
+import TextAreaInput from '../Input/TextAreaInput'
+import TextInput from '../Input/TextInput'
+
+interface CardBodyProps {
+  type: 'select' | 'text' | 'textarea'
+  options: QuestionOptionsType
+}
+const CardBody = ({ type, options }: CardBodyProps) => {
+  let InputComponent: React.ElementType | null = null
+  if (type === 'select') {
+    InputComponent = SelectInput
+  } else if (type === 'text') {
+    InputComponent = TextInput
+  } else if (type === 'textarea') {
+    InputComponent = TextAreaInput
+  }
+
+  return InputComponent ? <InputComponent options={options} /> : null
+}
+
+export default CardBody
